1774b373522d2a2b45921df6ccb06765a295e5bf
Adrian Ugray <adrian.ugray@datarwe.com>
2025-10-31 17:19:32 +1100

    Add outcomes

---
+++ graphing/dbt/demo/models/estimation/estimation_18_outcomes.sql
@@ -0,0 +1,17 @@
+WITH base AS (
+    SELECT RESULT_TIME,name,type,subject_age,verification_status,reported_age,time_in_seconds,actually_allowed,estimated_allowed
+    FROM {{ ref('estimation_18_flags') }}
+    UNION ALL
+    SELECT RESULT_TIME,name,type,subject_age,verification_status,reported_age,time_in_seconds,actually_allowed_18 as actually_allowed,estimated_allowed_18 as estimated_allowed
+    FROM {{ ref('estimation_0_flags') }}
+)
+
+SELECT *,
+    CASE
+        WHEN actually_allowed = TRUE  AND estimated_allowed = TRUE  THEN 'TP'
+        WHEN actually_allowed = FALSE AND estimated_allowed = FALSE THEN 'TN'
+        WHEN actually_allowed = FALSE AND estimated_allowed = TRUE  THEN 'FP'
+        WHEN actually_allowed = TRUE  AND estimated_allowed = FALSE THEN 'FN'
+        ELSE 'Indeterminate'
+    END AS outcome
+FROM base
