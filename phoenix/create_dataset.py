66a918b3d1f16eb1ce968c59f4366c43718e9c32
Joe Burton <joe.burton@kjr.com.au>
2025-11-28 04:25:22 +0000

    Update create_dataset script

--- phoenix/create_dataset.py
+++ phoenix/create_dataset.py
@@ -1,12 +1,12 @@
 from client import client
-import asyncio
 
-async def main():
-    dataset = await client.datasets.create_dataset(
-        csv_file_path="../eval/evaluations/ragas/datasets/dataset1.csv",
-        name="test-dataset",
-        input_keys=["user_input"],
-        output_keys=["reference"]
-    )
+DATASET_NAME: str = "my-dataset"
+DATASET_PATH: str = "../eval/evaluations/ragas/datasets/dataset1.csv"
 
-asyncio.run(main())
+dataset = client.datasets.create_dataset(
+    csv_file_path="../eval/evaluations/ragas/datasets/dataset1.csv",
+    name=DATASET_NAME,
+    input_keys=["user_input"], # corresponds to input parameter in experiments
+    output_keys=["reference"] # corresponds to reference parameter in experiments
+)
+print(dataset)
